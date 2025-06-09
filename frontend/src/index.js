cd4e45246c4db9e4f3aee02174c99981a32bac0e
Siddesh Vilas Pawar <siddeshpawar622.sp@gmail.com>
2025-06-09 17:50:10 +0100

    something

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AppWithProvider } from './App'; // Import the wrapped App component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppWithProvider />
   </React.StrictMode>
 );
 
